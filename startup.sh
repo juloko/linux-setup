33fc0746323a69263fe686feb4e6e188b3020c08
Melisse Cabral <melissecabral@gmail.com>
2020-09-18 19:26:18 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -166,3 +166,8 @@ sudo snap install --classic heroku
 
 echo 'installing firebase CLI'
 curl -sL https://firebase.tools | bash
+
+echo 'installing yarn'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn -y
